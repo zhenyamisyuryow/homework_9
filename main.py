9d245d56a3ff1a065339982cb12fd826f747fba6
Eugene M <eugene.misyuryow@gmail.com>
2023-07-04 16:03:47 +0300

    homework done

---
+++ main.py
@@ -0,0 +1,76 @@
+contacts = {}
+
+
+def input_error(func):
+    def handler(*args):
+        try:
+            return func(*args)
+        except (KeyError, ValueError, IndexError) as e:
+            if type(e) == KeyError:
+                return f"Error: key doesn't exist."
+            elif type(e) == ValueError:
+                return "Error: phone can only contain digits."
+            elif type(e) == IndexError:
+                return "Error: provide both name and phone number."
+    return handler
+
+@input_error
+def hello(*args):
+    return "Hi! How can I help you today?"
+
+@input_error
+def add(*args) -> None:
+    phone = int(args[1])
+    name = args[0]
+    contacts[name] = phone
+    return f"Success! {name} has been added to your contacts list."
+
+@input_error
+def change(name:str, phone:int):
+    phone = int(phone)
+    if name in contacts:
+        contacts[name] = phone
+        return f"Success! Phone number for {name} has been changed."
+    else:
+        return f"{name} was not found in contacts list."
+
+@input_error
+def phone(name: str):
+    return contacts[name]
+
+@input_error
+def showall(*args):
+    result = []
+    for k,v in contacts.items():
+        result.append(': '.join([k,str(v)]))
+    return '\n'.join(result)
+    
+def main():
+
+    func_maps = {
+        "hello" : hello,
+        "add" : add,
+        "change" : change,
+        "phone" : phone,
+        "showall" : showall
+    }
+
+    print("Enter the first command:")
+    
+    while True:
+
+        user_input = input(">>> ").lower()
+
+        if user_input in ["good bye", "exit", "close"]:
+            print("Good bye!")
+            break
+
+        input_parts = user_input.split()
+        command = input_parts[0]
+        args = input_parts[1:]
+
+        if command in func_maps:
+            print(func_maps[command](*args))
+
+if __name__ == "__main__":
+    main()
